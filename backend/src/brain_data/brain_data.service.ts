b9c5d9bb07e7dee70c76e031ac04867674f1cbcf
Hazim Ab Halim <hazim.halim54@yahoo.com>
2023-06-28 15:30:02 -0400

    set type validation on client request

--- backend/src/brain_data/brain_data.service.ts
+++ backend/src/brain_data/brain_data.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { FilterBrainData } from './dto/filter_brain_data.dto';
 import { PrismaClient } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { SummaryBrainData } from './dto/summary_brain_data.dto';
 
 @Injectable()
 export class BrainDataService {
-  getSummary(filter: FilterBrainData){
 
+  constructor(private prisma: PrismaService) {}
+
+  getSummary(filter: FilterBrainData){
+    //use prisma to get data
+    const summaryObj = new Array<SummaryBrainData>;
+    return this.prisma.brain_data.findMany();
   }
 }
