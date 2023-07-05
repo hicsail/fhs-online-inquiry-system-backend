2a1ebb2756ec90fb76ea5526b7052ba177bcc489
Hazim Ab Halim <hazim.halim54@yahoo.com>
2023-07-05 16:50:43 -0400

    changed enum, deleted dto, made initial queries

--- backend/src/brain_data/brain_data.service.ts
+++ backend/src/brain_data/brain_data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { FilterBrainData } from './dto/filter_brain_data.dto';
+import { FilterBrainData, Categories } from './dto/filter_brain_data.dto';
 import { PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { SummaryBrainData } from './dto/summary_brain_data.dto';
@@ -9,9 +9,37 @@ export class BrainDataService {
 
   constructor(private prisma: PrismaService) {}
 
-  getSummary(filter: FilterBrainData){
+  async getSummary(filter: FilterBrainData){
     //use prisma to get data
-    const summaryObj = new Array<SummaryBrainData>;
-    return this.prisma.brain_data.findMany();
+
+    const sex_type = [1,2];
+    const summaryArr = new Array<SummaryBrainData>;
+    const test = await this.prisma.brain_data.findMany({
+      where:{
+        // where all the input with ranges should go
+        AND:[
+          { age_core1 : filter.age_range ? { gte: filter.age_range[0]} : {} },
+          { age_core1 : filter.age_range ? { lte: filter.age_range[1]} : {}},
+          { age_death: filter.age_at_death_range ? { gte: filter.age_at_death_range[0] } : {} },
+          { age_death: filter.age_at_death_range ? { lte: filter.age_at_death_range[1] } : {} },
+          { nprin: filter.rna_integrity ? { gte: filter.rna_integrity[0] } : {} },
+          { nprin: filter.rna_integrity ? { gte: filter.rna_integrity[1] } : {} },
+          { nppmih_hours: filter.postmortem_interval_hours ? { gte: filter.postmortem_interval_hours[0]} : {} },
+          { nppmih_hours: filter.postmortem_interval_hours ? { lte: filter.postmortem_interval_hours[1]} : {} },
+        ]
+      },
+      include:{
+        participants: {
+          select: {
+            edu_core2: true
+          }
+        }
+      }
+    });
+
+    
+
+
+    return test;
   }
 }
--- backend/src/brain_data/dto/filter_brain_data.dto.ts
+++ backend/src/brain_data/dto/filter_brain_data.dto.ts
@@ -1,50 +1,60 @@
 import { IsInt, IsEnum, IsOptional} from "class-validator";
-
+import { ApiBody, ApiProduces, ApiProperty } from "@nestjs/swagger";
 
 // more categories can be added when options are finalized
-enum Categories {
-    "Frozen Tissue Present",
-    "Fixative",
-    "Chronic Traumatic Encephalopathy",
-    "Atherosclerosis Severity",
-    "ALS/Motor Neuron Disease",
-    "Derived AD Dementia"    
+export enum Categories {
+    "Frozen Tissue Present" = "npfrotispre",
+    "Fixative" = "npfix",
+    "Observed infarcts" = "npinf",
+    "Chronic Traumatic Encephalopathy" = "npftdt7",
+    "Atherosclerosis Severity" = "npavas",
+    "ALS/Motor Neuron Disease" = "pathmnd",
+    "Derived AD Dementia" = "pathad",
+    "Age-related Tauopathy" = "relatauo",
+    "FTLD with Tau Pathology" = "npftdtau",
+    "FTLD with TDP-43" = "npftdtdp",
+    "Hippocampal Sclerosis" = "nphipscl"     
 }
 
 
 export class FilterBrainData {
     
+    @ApiProperty({ type: [Number], example: [20,24], description: 'Type is an array of integers. Filters the age of participants'})
     @IsOptional()
     @IsInt({
         each: true,
-        
     })
     age_range: number[];
     
+    @ApiProperty({ type: [Number], example: [25,51], description: 'Type is an array of integers. Filters BMI of participants'})
     @IsOptional()
     @IsInt({
         each: true
     })
     bmi_range: number[];
     
+    @ApiProperty({ type: [Number], example: [25,51], description: 'Type is an array of integers. Filters postmortem hours of participants'})
     @IsOptional()
     @IsInt({
         each: true
     })
     postmortem_interval_hours: number[];
     
+    @ApiProperty({ type: [Number], example: [25,51], description: 'Type is an array of integers. Filters RNA integrity of participants'})
     @IsOptional()
     @IsInt({
         each: true
     })
     rna_integrity: number[];
     
+    @ApiProperty({ type: [Number], example: [25,51], description: 'Type is an array of integers. Filters age at death of participants'})
     @IsOptional()
     @IsInt({
         each: true
     })
     age_at_death_range: number[];
     
+    @ApiProperty({ type: [String], example: ["Fixative", "Frozen Tissue Present"], description: 'Type is an array of enum Categories.'})
     @IsOptional()
     @IsEnum(Categories, {
         each: true
