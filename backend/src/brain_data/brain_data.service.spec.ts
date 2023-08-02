bec291ffc28b347f2dc1de0774dd3d3c1ff4ed6a
Ned Searls <nsearls@bu.edu>
2023-08-02 14:19:26 -0400

    Testing for brain-data endpoint - approximately 5 tests have failed

--- backend/src/brain_data/brain_data.service.spec.ts
+++ backend/src/brain_data/brain_data.service.spec.ts
@@ -1,70 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpModule } from '@nestjs/axios';
+import { HttpService } from '@nestjs/axios';
+import * as request from 'supertest';
 import { BrainDataService } from './brain_data.service';
 import { BrainDataController } from './brain_data.controller';
 import { PrismaService } from '../prisma.service';
 import { FilterBrainData } from './dto/filter_brain_data.dto';
+import { BrainDataModule } from './brain_data.module';
 import { sum } from 'mathjs';
+import { HttpStatus } from '@nestjs/common';
+import { SummaryBrainData } from './dto/summary_brain_data.dto';
+import { INestApplication } from '@nestjs/common';
+const filters = [
+  {
+    age_core1: [20, 24],
+    nppmih_hours: [25, 51],
+    nprin: [25, 51],
+    age_death: [25, 51],
+    categories: { npfrotispre: 3 },
+  },
+
+  { age_core1: [-20, 24] },
+
+  { age_core1: [21, 29] },
+  { bmi: [15, 29] },
+  { age_core1: [1] },
+  { age_core1: ['1'] },
+  { age_core1: [1, Number.MAX_SAFE_INTEGER + 1] },
+  { age_core1: Array(2 ** 16 - 1).fill(0) },
+
+  { nprin: [Number.MAX_SAFE_INTEGER * -1 - 1, Number.MAX_SAFE_INTEGER + 1] },
+  { categories: { npfrotispre: Number.MAX_SAFE_INTEGER + 1 } },
+  { categories: { npfrotispre: -1 } },
+  { categories: { npfrotispre: -1.0 } },
+  { categories: { relatauo: [1] } },
+  { categories: { relatauo: 'string' } },
+];
 
 describe('BrainDataService', () => {
   let service: BrainDataService;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [BrainDataController],
-      providers: [BrainDataService, PrismaService],
+      imports: [BrainDataModule],
     }).compile();
 
     service = module.get<BrainDataService>(BrainDataService);
+    app = module.createNestApplication();
+    await app.init();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
   describe('summaryResults', () => {
     const threshold = 5;
-    const filter = new FilterBrainData();
 
-    filter.age_core1 = [20, 24];
-    filter.bmi = [25, 51];
-    filter.nppmih_hours = [25, 51];
-    filter.nprin = [25, 51];
-    filter.age_death = [25, 51];
-    // filter.categories = { npfrotispre: 3 };
-
-    const entries = Object.entries(filter);
-    const out = entries.reduce((arr, group) => {
-      const itms = Object.values(group)[1];
-      const vals = Object.values(itms);
-      // if first time, set up the initial arrays with first set of data
-      if (!arr.length) {
-        return vals.map((v) => [v]);
-      }
-      // after first one, we will just loop over the arrays
-      // and start adding the next set of data to each one
-      return arr.reduce<number[][]>((updated, curr) => {
-        vals.forEach((val: number) => {
-          // make copy so we are not adding data to the reference
-          const copy = (<number[]>curr).slice();
-          copy.push(val);
-          updated.push(copy);
-        });
-        return updated;
-      }, []);
-    }, []);
-    console.log(out);
-    const filterPermutations: FilterBrainData[] = [filter];
+    // It would be convenient to have a constructor for the FilterBrainData class
+    const filterPermutations: FilterBrainData[] = filters.map((filter) => {
+      const brainFilter = new FilterBrainData();
+      Object.entries(filter).map(([key, value]) => {
+        brainFilter[key as keyof FilterBrainData] = value;
+      });
+      return brainFilter;
+    });
 
     test.each(filterPermutations)(
       'if not 206 should be greater than threshold',
       async (filter) => {
-        const summaryResults = await service.getSummary(filter);
-        console.log(summaryResults);
-        summaryResults.map((row) => {
-          expect(row['total']).toBeGreaterThanOrEqual(threshold);
-        });
+        const res = await request(app.getHttpServer())
+          .post('/brain-data')
+          .send(filter)
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json');
+        const summaryResults = res.body as SummaryBrainData[];
+        const status = res.statusCode;
+
+        if (status !== HttpStatus.PARTIAL_CONTENT) {
+          expect(status).toBe(HttpStatus.CREATED);
+          if (summaryResults != undefined) {
+            summaryResults.map((row) => {
+              // If function resolves - can be =0 OR >=5
+              try {
+                expect(row['total']).toBeGreaterThanOrEqual(threshold);
+              } catch {
+                expect(row['total']).toBe(0);
+              }
+            });
+          }
+        } else {
+          expect(status).toBe(HttpStatus.PARTIAL_CONTENT);
+        }
       },
     );
+    afterAll(async () => {
+      await app.close();
+    });
   });
 });
